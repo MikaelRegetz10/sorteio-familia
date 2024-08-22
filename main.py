118666bb918d6b49da7c05605935b98d02e9b19f
MikaelRegetz10 <mikaelregetz@gmail.com>
2024-08-22 19:23:22 -0300

    Refatoração de código e da lógica do sorteio

--- main.py
+++ main.py
@@ -2,62 +2,37 @@ import random
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from typing import Tuple, List, Dict
+import settings.setting as settings
 
-# Dados das famílias
-familia = {
-    "regetz": {
-        "email": "aldabrasilia@gmail.com",
-        "familia": ["Mikael Regetz", "Isabelly Regetz", "Miguel Regetz", "Alda Oliveira"]
-    },
-    "zimmermann": {
-        "email": "cassiabrasilia@gmail.com",
-        "familia": ["Cássia Zimmermann", "Rosino Estevam", "Salomão Zimmermann", "Serena Zimmermann"]
-    },
-    
-    "henrique": {
-        "email": "alanna.bsb.df@gmail.com",
-        "familia": ["Alanna Zimmermann", "Ericatia Zimmermann", "Fábio Henrique"]
-    },
 
-    "soares": {
-        "email": "lindalvasoaresr@gmail.com",
-        "familia": ["Carlos", "Samuel", "Lindalva"]
-    },
+def lista_pessoas_e_familia(familia: dict) -> List[Tuple[str, str]]:
+    pessoas = [] # (nome pessoas, familia)
+    for sobrenome, dados in familia.items():
+        for membro in dados["familia"]:
+            pessoas.append((membro, sobrenome))
+    return pessoas
 
-    "cassia": {
-        "email": "demetrio.pereira1968@gmail.com",
-        "familia": ["Ericarla", "Demetrio", "Abraão"]
-    },
-}
 
-pessoas = []
-for sobrenome, dados in familia.items():
-    for membro in dados["familia"]:
-        pessoas.append((membro, sobrenome))
-
-# Função para realizar o sorteio
-def sorteio_entre_familias(pessoas):
+def sorteio_entre_familias(pessoas: list) -> Dict[str,str]:
     resultado = {}
     disponiveis = pessoas.copy()
     
     for pessoa, familia_pessoa in pessoas:
-        # Tentar sortear alguém de uma família diferente
         candidatos = [p for p in disponiveis if p[1] != familia_pessoa]
         
         if not candidatos:
-            # Se não houver candidatos, reiniciar o sorteio
             return sorteio_entre_familias(pessoas)
         
         sorteado = random.choice(candidatos)
         resultado[pessoa] = sorteado[0]
         disponiveis.remove(sorteado)
-    
     return resultado
 
-# Função para enviar email
-def enviar_email(destinatario, assunto, corpo):
-    remetente = ""
-    senha = ""
+
+def enviar_email(destinatario: str, assunto: str, corpo: str) -> None:
+    remetente = settings.EMAIL
+    senha = settings.SENHA
 
     msg = MIMEMultipart()
     msg['From'] = remetente
@@ -75,18 +50,32 @@ def enviar_email(destinatario, assunto, corpo):
         server.quit()
 
         print(f"Email enviado para {destinatario}")
+
     except Exception as e:
         print(f"Falha ao enviar email para {destinatario}: {e}")
 
-# Executar o sorteio
-resultado_sorteio = sorteio_entre_familias(pessoas)
 
-# Preparar e enviar emails para cada família
-for sobrenome, dados in familia.items():
-    corpo_email = "Resultado do sorteio:\n\n"   
-    for pessoa in dados["familia"]:
-        sorteado = resultado_sorteio[str(pessoa)]
-        corpo_email += f"{pessoa} tirou {sorteado}\n"
+def sorteio_main(familia: dict) -> Dict[str, bool]:
+    try:
+        print(familia)
+        pessoas = lista_pessoas_e_familia(familia)
+        resultado_sorteio = sorteio_entre_familias(pessoas)
 
-    print(corpo_email)
-    #enviar_email(dados["email"], "Resultado do Sorteio de Natal", corpo_email)
+        for sobrenome, dados in familia.items():
+            corpo_email = "Resultado do sorteio:\n\n"
+            for pessoa in dados["familia"]:
+                sorteado = resultado_sorteio[str(pessoa)]
+                corpo_email += f"{pessoa} tirou {sorteado}\n"
+
+            enviar_email(dados["email"], "Resultado do Sorteio de Natal", corpo_email)
+            print("email")
+
+        return {"status": True}
+
+    except Exception as e:
+        print(f"Erro: {e}")
+        return {"status": False}
+
+if __name__ == '__main__':
+    familia = {'mikaelregetz': {'email': 'mikaelregetz@gmail.com', 'familia': ['mikael', 'isa']}, 'tanossegundo': {'email': 'tanossegundo@gmail.com', 'familia': ['a', 'b']}}
+    sorteio_main(familia)
